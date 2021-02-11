1c9bf9ca643ee76736742af3d08477ff1e80fb1f
AlexYankoff <alex.yankoff@gmail.com>
2021-02-11 16:22:12 +0200

    ForEach test

---
+++ JS Advanced/08.Classes/test.js
@@ -0,0 +1,16 @@
+class Circle {
+        constructor(r) {
+            this.r = r;
+            this.area =7
+        }
+    };
+
+    circle5 = new Circle(5)
+    
+
+    Circle.area = 11
+
+    console.log(circle5)
+    console.log(circle5)
+
+    
