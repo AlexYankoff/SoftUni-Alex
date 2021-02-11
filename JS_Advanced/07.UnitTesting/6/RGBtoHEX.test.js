a98dac4354844b43a823e240e7c5e81d39ae9c61
AlexYankoff <alex.yankoff@gmail.com>
2021-02-11 20:13:17 +0200

    Change directory

--- JS_Advanced/07.UnitTesting/6/RGBtoHEX.test.js
+++ JS_Advanced/07.UnitTesting/6/RGBtoHEX.test.js
@@ -20,34 +20,12 @@ it ('return undefined if parameter is not in range', () => {
     expect(rgbToHexColor(200,256,255)).to.equal(undefined); 
     expect(rgbToHexColor(200,-1,255)).to.equal(undefined); 
     expect(rgbToHexColor(256,255,256)).to.equal(undefined); 
-    expect(rgbToHexColor(256,255,-1)).to.equal(undefined); 
-    describe('test RGBtoHEX', () => {
-//Take three integer numbers, representing the red, green and blue values of an RGB color, each within range [0…255]
-it('converts black to hex', () => {
-    expect(rgbToHexColor(0,0,0)).to.equal('#000000');
-
-})
-
-it('converts white to hex', () => {
-    expect(rgbToHexColor(255,255,255)).to.equal('#FFFFFF');
-
-})
-
-it ('return undefined if parameter is not in range', () => {
-    expect(rgbToHexColor(256,255,255)).to.equal(undefined);
-    expect(rgbToHexColor(-1,255,255)).to.equal(undefined); 
-    expect(rgbToHexColor(200,256,255)).to.equal(undefined); 
-    expect(rgbToHexColor(200,-1,255)).to.equal(undefined); 
-    expect(rgbToHexColor(256,255,256)).to.equal(undefined); 
-    expect(rgbToHexColor(256,255,-1)).to.equal(undefined); 
-    expect(rgbToHexColor('a','b','c')).to.equal(undefined); 
-
-    
-});
+    expect(rgbToHexColor(255,255,-1)).to.equal(undefined); 
+    expect(rgbToHexColor('a',211,222)).to.equal(undefined); 
+    expect(rgbToHexColor(256,'b', 222)).to.equal(undefined); 
+    expect(rgbToHexColor(256,255,'c')).to.equal(undefined); 
 
 
-
-})
 
     
 });
---
+++ JS_Advanced/08.Classes/EXERCISES/test.js
@@ -0,0 +1,14 @@
+const arr = ["one|едно","two|две","three|три"];
+let newArr = []
+let spl = ''
+arr.forEach((el,i) =>add)
+
+function add(el,i) {
+    spl = el.split('|')
+    arr[index]= spl
+}
+  
+
+console.log (newArr)
+
+
