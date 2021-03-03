fa227a754995a9e555ded33db7b0209da731bf50
AlexYankoff <alex.yankoff@gmail.com>
2021-03-03 07:36:41 +0200

    Locked-Profile
    Working fine

--- JS_App/02.Async_programming/EXERCISES/04.Locked-Profile/app.js
+++ JS_App/02.Async_programming/EXERCISES/04.Locked-Profile/app.js
@@ -4,16 +4,22 @@ function lockedProfile() {
 
     function onClick(ev) {
         if (ev.target.tagName == 'BUTTON') {
-            console.log('click')
             const profile = ev.target.parentNode;
+            console.log(profile)
+            
+            
+            
             let isLocked = profile.querySelector('input[type=radio]:checked').value =='lock';
             if (isLocked) {return;}
         
         let div = profile.querySelector('div');
-        let isVusible = div.style.display 
-        profile.querySelector('BUTTON').textContent = isVusible ? 'Show more' : 'Hide it'
-        div.style.display = isVusible ? '' : 'block'
-            
+        
+        let isVisible = div.className 
+        console.log(isVisible)
+     
+        profile.querySelector('BUTTON').textContent = isVisible=='hidden' ? 'Hide it' : 'Show more'
+        div.className = isVisible=='hidden' ? 'visible' : 'hidden'
+           
         
         }
     }
@@ -46,12 +52,12 @@ function createElement(profile) {
         e('hr'),
         e('label',{},'Username'),
         e('input',{type:'text', name:'user1Username',value:profile.username, disabled:true,readOnly:true,}),
-        e('div',{id:'user1HiddenFields'}, 
+        e('div',{className:'hidden'}, 
             e('hr'),
             e('label',{},'Email'),
-            e('input',{type:'email', name:'user1Email',disabled:true,readOnly:true,}),
+            e('input',{type:'email', value:profile.email, disabled:true,readOnly:true,}),
             e('label',{},'Age'),
-            e('input',{type:'email', name:'user1Age',disabled:true,readOnly:true,})),
+            e('input',{type:'email', value:profile.age,disabled:true,readOnly:true,})),
         e('button',{},'Show more')
         
         )
