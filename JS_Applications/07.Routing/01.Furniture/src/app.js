633e5a106b9350d661d2b74bc54c43abbe042bc6
AlexYankoff <alex.yankoff@gmail.com>
2021-03-23 11:36:57 +0200

    01.Furniture  - login ready

--- JS_Applications/07.Routing/01.Furniture/src/app.js
+++ JS_Applications/07.Routing/01.Furniture/src/app.js
@@ -1,5 +1,7 @@
 // SET THE HOST BELOW
 import page from '../node_modules/page/page.mjs';
+import { render } from '../node_modules/lit-html/lit-html.js'
+
 import { dashboardPage } from './views/dashboard.js'
 import {myPage} from './views/myFurniture.js'
 import {detailsPage} from './views/details.js'
@@ -13,18 +15,38 @@ import * as api from './api/data.js';
 
 window.api = api;
 
-
+const main = document.querySelector('.container')// SET IT
 
 //routing table - изпълнява дадената ф-я и й подава context
-page('/', dashboardPage); //view Controller
-page('/my-furniture', myPage);
-page('/details/:id', detailsPage);// в контекста имаме и id
-page('/create', createPage); 
-page('/edit/:id',editPage);
-page('/register',registerPage);
-page('/login', loginPage);
+page('/', decorateContext, dashboardPage); //view Controller
+page('/my-furniture', decorateContext, myPage);
+page('/details/:id', decorateContext, detailsPage);// в контекста имаме и id
+page('/create', decorateContext, createPage); 
+page('/edit/:id', decorateContext,editPage);
+page('/register', decorateContext,registerPage);
+page('/login', decorateContext, loginPage);
 
+setUserNav() // викаме я при стартиране, ако вече има логнат потребител
+
+//Start application
 page.start();
+
+function decorateContext(ctx, next) {
+    ctx.render = (ctx) => render(ctx, main)
+    ctx.setUserNav = setUserNav
+    next()
+}
+
+function setUserNav() {
+    const userId = sessionStorage.getItem('userId');
+    if (userId !=null) {
+        document.getElementById('user').style.display = 'inline-block';
+        document.getElementById('guest').style.display = 'none'
+    } else {
+        document.getElementById('user').style.display = 'none';
+        document.getElementById('guest').style.display = 'inline-block'
+    }
+}
 
 
 
