8881707a45f51367b66b3d35b376c0eb4f816c2a
AlexYankoff <alex.yankoff@gmail.com>
2021-04-02 10:26:58 +0300

    Movies Ready
    Everything is working!

--- JS_Applications/11.Movies/src/views/create.js
+++ JS_Applications/11.Movies/src/views/create.js
@@ -1,9 +1,28 @@
 import {html} from '../../node_modules/lit-html/lit-html.js'
 import {createRecord} from '../api/data.js'
 
-// DON'T FORGET TO ADD @sumbit to form
-
+// DON'T FORGET TO ADD @submit =${onSubmit} to form
 const createTemplate = (onSubmit,invDesc) => html `
+<section id="add-movie">
+        <form @submit =${onSubmit} class="text-center border border-light p-5" action="#" method="">
+            <h1>Add Movie</h1>
+            <div class="form-group">
+                <label for="title">Movie Title</label>
+                <input type="text" class="form-control" placeholder="Title" name="title" value="">
+            </div>
+            <div class="form-group">
+                <label for="description">Movie Description</label>
+                <textarea class="form-control" placeholder="Description" name="description"></textarea>
+            </div>
+            <div class="form-group">
+                <label for="imageUrl">Image url</label>
+                <input type="text" class="form-control" placeholder="Image Url" name="img" value="">
+            </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+    </section>`;
+
+const OLDcreateTemplate = (onSubmit,invDesc) => html `
 <div class="row space-top">
             <div class="col-md-12">
                 <h1>Create New Furniture</h1>
@@ -60,40 +79,13 @@ export async function createPage (ctx) {
         const data = ([...formData.entries()].reduce((a, [k,v]) => Object.assign(a, {[k]:v}), {}));
         
         //VALIDATION IF ANY FIELD IS EMPTY (MATERIAL EXCLUDED)
-        let invMake = false;
-        let invModel =false;
-        let invDesc = false
-        let problem = false
+       
          
         if  (Object.entries(data).filter(([k, v]) => k!='material').some(([k,v]) => v=='')){
             return alert('Missing data!');
         }
-        if (data.make.length<4){
-             invMake = true;
-             problem = true;
-            alert ('Make must be at least 4 symbols long!')
-        } else {invMake = false};
-        if (data.model.length<4){
-            invModel = true;
-            problem = true;
-            alert ('Model must be at least 4 symbols long!')
-        } else (invModel= false);
-        if (data.year<1950 ||data.year>2050){
-            alert ('Year must be between 1950 and 2050')
-        }
-        if (data.description.length<11){
-            invDesc = true
-            alert ('Description must be more than 10 symbols')
-        }
-        if (data.price<0){
-            alert ('Price must be a positive number')
-        } 
-        if (problem) {
-            return
-        } 
+        
 
-    data.year = Number(data.year);
-    data.price = Number(data.price); 
     await createRecord(data)
 
     ctx.page.redirect('/');
