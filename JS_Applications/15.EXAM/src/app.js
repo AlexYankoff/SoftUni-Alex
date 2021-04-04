a339f5d8b7cf58a2464b0dff6a6e73fada0d7476
AlexYankoff <alex.yankoff@gmail.com>
2021-04-04 09:02:40 +0300

    theStart

---
+++ JS_Applications/15.EXAM/src/app.js
@@ -0,0 +1,67 @@
+// SET THE MAIN  BELOW
+console.log('app.js started')
+
+
+import page from '../node_modules/page/page.mjs';
+import { render } from '../node_modules/lit-html/lit-html.js'
+
+import { homePage } from './views/home.js'
+import { dashboardPage } from './views/dashboard.js'
+import {logout } from './api/data.js'
+import {myPage} from './views/myview.js'
+import {detailsPage} from './views/details.js'
+import {createPage} from './views/create.js'
+import {editPage} from './views/edit.js'
+import {registerPage} from './views/register.js'
+import {loginPage} from './views/login.js'
+//
+import * as api from './api/data.js'; // за тестване на функции
+window.api = api;                      // в броузер
+//
+const main = document.querySelector('main')// SET IT
+//
+////routing table - изпълнява дадената ф-я и й подава context
+page('/', decorateContext, homePage)
+page('/dashboard', decorateContext, dashboardPage); //view Controller
+page('/myview', decorateContext, myPage);
+page('/details/:id', decorateContext, detailsPage);// в контекста имаме и id
+page('/create', decorateContext, createPage); 
+page('/edit/:id', decorateContext,editPage);
+page('/register', decorateContext,registerPage);
+page('/login', decorateContext, loginPage);
+//
+//LOGOUT
+//document.getElementById('logoutBtn').addEventListener('click',async () => {
+//    await logout();
+//    setUserNav() //ъпдейтваме навигацията при logout!
+//    page.redirect('/')
+//})
+//
+setUserNav() // викаме я при стартиране, ако вече има логнат потребител
+//
+//Start application
+page.start();
+//
+function decorateContext(ctx, next) {
+    console.log('enter decorate')
+    ctx.render = (ctx) => render(ctx, main)
+    ctx.setUserNav = setUserNav
+    next()
+}
+//
+function setUserNav() {
+    console.log( "need to setup Nav")
+    //const userId = sessionStorage.getItem('userId');  //или authToken или email, едно и също е
+    //if (userId !=null) {
+    //    document.getElementById('user').style.display = 'inline-block';
+    //    document.getElementById('guest').style.display = 'none'
+    //} else {
+    //    document.getElementById('user').style.display = 'none';
+    //    document.getElementById('guest').style.display = 'inline-block'
+    //}
+}
+
+
+
+
+
---
+++ JS_Applications/15.EXAM/src/views/register.js
@@ -0,0 +1,62 @@
+import { html } from '../../node_modules/lit-html/lit-html.js';
+import { register } from '../api/data.js'
+
+// DON'T FORGET TO ADD @submit = ${onSubmit} to form
+
+const registerTemplate = (onSubmit,invalidEmail, invalidPassword,invalidRe )=> html `
+<div class="row space-top">
+            <div class="col-md-12">
+                <h1>Register New User</h1>
+                <p>Please fill all fields.</p>
+            </div>
+        </div>
+        <form @submit = ${onSubmit}>
+            <div class="row space-top">
+                <div class="col-md-4">
+                    <div class="form-group">
+                        <label class="form-control-label" for="email">Email</label>
+                        <input class= ${'form-control'+ (invalidEmail ? ' is-invalid' : '')}  id="email" type="text" name="email">
+                    </div>
+                    <div class="form-group">
+                        <label class="form-control-label" for="password">Password</label>
+                        <input class=${'form-control'+ (invalidPassword ? ' is-invalid' : '')} id="password" type="password" name="password">
+                    </div>
+                    <div class="form-group">
+                        <label class="form-control-label" for="rePass">Repeat</label>
+                        <input class=${'form-control'+ (invalidRe ? ' is-invalid' : '')} type="password" name="rePass">
+                    </div>
+                    <input type="submit" class="btn btn-primary" value="Register" />
+                </div>
+            </div>
+        </form>
+    </div>`;
+
+
+export async function registerPage (ctx) {
+    console.log('start register')
+    ctx.render(registerTemplate(onSubmit))
+
+    async function onSubmit(event) {
+        event.preventDefault();
+        const formData = new FormData(event.target);
+        const email = formData.get('email');
+        const password = formData.get('password').trim();
+        const repass = formData.get('rePass').trim()
+    
+        if (email== ''|| password== ''|| repass== '') {
+            ctx.render(registerTemplate(onSubmit, email== '', password== '', repass== '' ))
+            return alert('All fields are required');
+        }
+        
+        if (password!=repass) {
+            ctx.render(registerTemplate(onSubmit, email== '',true, true))
+            return alert('Password do\'t macth!')
+        }
+    
+        await register (email, password)
+
+        ctx.setUserNav()
+        ctx.page.redirect('/')
+    
+    }
+    }
