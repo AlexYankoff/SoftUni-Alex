a390d383e389dba656d57e76478e203442b7591a
AlexYankoff <alex.yankoff@gmail.com>
2021-04-04 09:34:53 +0300

    login and register ready

--- JS_Applications/15.EXAM/src/views/register.js
+++ JS_Applications/15.EXAM/src/views/register.js
@@ -4,6 +4,36 @@ import { register } from '../api/data.js'
 // DON'T FORGET TO ADD @submit = ${onSubmit} to form
 
 const registerTemplate = (onSubmit,invalidEmail, invalidPassword,invalidRe )=> html `
+<section id="register-page" class="content auth">
+            <h1>Register</h1>
+
+            <form @submit = ${onSubmit} id="register" action="#" method="">
+                <fieldset>
+                    <blockquote>Knowledge is not simply another commodity. On the contrary. Knowledge is never used up.
+                        It
+                        increases by diffusion and grows by dispersion.</blockquote>
+                    <p class="field email">
+                        <label for="register-email">Email:</label>
+                        <input type="email" id="register-email" name="email" placeholder="maria@email.com">
+                    </p>
+                    <p class="field password">
+                        <label for="register-pass">Password:</label>
+                        <input type="password" name="password" id="register-pass">
+                    </p>
+                    <p class="field password">
+                        <label for="register-rep-pass">Repeat password:</label>
+                        <input type="password" name="rep-pass" id="register-rep-pass">
+                    </p>
+                    <p class="field submit">
+                        <input class="btn submit" type="submit" value="Register">
+                    </p>
+                    <p class="field">
+                        <span>If you already have profile click <a href="#">here</a></span>
+                    </p>
+                </fieldset>
+            </form>
+        </section>`;
+const OLDregisterTemplate = (onSubmit,invalidEmail, invalidPassword,invalidRe )=> html `
 <div class="row space-top">
             <div class="col-md-12">
                 <h1>Register New User</h1>
@@ -41,7 +71,7 @@ export async function registerPage (ctx) {
         const formData = new FormData(event.target);
         const email = formData.get('email');
         const password = formData.get('password').trim();
-        const repass = formData.get('rePass').trim()
+        const repass = formData.get('rep-pass').trim()
     
         if (email== ''|| password== ''|| repass== '') {
             ctx.render(registerTemplate(onSubmit, email== '', password== '', repass== '' ))
